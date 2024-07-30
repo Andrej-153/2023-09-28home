e19bb1e0f42fe5c1491135bcd1a853971cea0d9f
Andrej-153 <andrejmaslov11143@gmail.com>
2024-07-30 12:10:14 +0300

    Add files via upload
    домашняя по словарям

---
+++ homerwork6/pythonProject2/pythonProjecHome6.py
@@ -0,0 +1,17 @@
+my_dict={'Anna' : 1995 ,'Petr' :1975 ,'Grigory': 2020 }
+print(my_dict)
+print(my_dict.keys())
+print(my_dict.get('Anna'))
+print(my_dict.get('Avraam'))
+my_dict.update ({'Avraam' : -3000 , 'Saara' : -2995})
+a_ = my_dict.pop('Petr')
+print(my_dict)
+print(a_)
+
+my_set={'Primus',1,2,1,2,3,5,8, True, 2.15 , True}
+print(my_set)
+my_set.add(False )
+my_set.add('kerogaz')
+
+print(my_set)
+
